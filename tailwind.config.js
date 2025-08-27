09dc038dc9447633538a814f3236459890263dc2
chabda01 <john-ross.adoko@epitech.eu>
2025-08-27 16:36:13 +0100

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,15 @@ module.exports = {
       fontFamily: {
         sans: ["var(--font-poppins)", ...fontFamily.sans],
       },
+      keyframes: {
+        'bounce-slow': {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-12px)' }, // monte de 12px
+        },
+      },
+      animation: {
+        'bounce-slow': 'bounce-slow 3s ease-in-out infinite', // animation lente
+      },
     },
   },
   plugins: [],
